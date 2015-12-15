6823b6d3e088100d5ed13628ec96cd7dc3cca83b
Angela527 <374536021@qq.com>
2015-12-15 15:43:18 +0800

    Initial Commit

---
+++ KikiQueen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KikiQueen
+//
+//  Created by Tagcare on 15/12/15.
+//  Copyright © 2015年 Tagcare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KikiQueen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KikiQueen
+//
+//  Created by Tagcare on 15/12/15.
+//  Copyright © 2015年 Tagcare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
